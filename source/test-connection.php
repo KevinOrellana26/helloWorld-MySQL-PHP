01bb11cb2d1d4b57d50e0cb0fdd37b87ed1b536d
KevinOrellana26 <kevinorellana202@gmail.com>
2023-08-14 16:36:03 +0200

    update

--- source/test-connection.php
+++ source/test-connection.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World from MySQL</title>
+    <title>test-connection</title>
 </head>
 <body>
     <?php
