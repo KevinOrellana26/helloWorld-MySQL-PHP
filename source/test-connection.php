1beafb9c6151bdaea56bd6cec50eea58ec2a00a1
KevinOrellana26 <kevinorellana202@gmail.com>
2023-08-16 10:13:47 +0200

    update

--- source/test-connection.php
+++ source/test-connection.php
@@ -7,12 +7,28 @@
 </head>
 <body>
     <?php
-    $mysqli = new mysqli("mysql", "user", "password", "mydb");
-    if ($mysqli->connect_error) {
-        echo "Error al realizar la conexión";
+    $host  = "mysql"
+    $port = "3306"
+    $user = "user"
+    $pass = "user"
+    $db = "mydb"
+
+    $conn = new mysqli($host, $user, $pass, $db, $port);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
-    echo "connected successfully";
-    $mysqli->close();
+
+    echo "Connected successfully to MySQL";
+
+    $conn->close();
+
+    // $mysqli = new mysqli("mysql", "user", "password", "mydb");
+    // if ($mysqli->connect_error) {
+    //     echo "Error al realizar la conexión";
+    // }
+    // echo "connected successfully";
+    
     ?>
 </body>
 </html>
