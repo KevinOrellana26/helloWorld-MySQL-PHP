5bfb00118056dda04ee76d3bbd6f8a93cd6ff7e5
KevinOrellana26 <kevinorellana202@gmail.com>
2023-08-16 13:53:43 +0200

    update

--- mysql/helloWorld.sql
+++ mysql/helloWorld.sql
@@ -1,10 +1,3 @@
-CREATE DATABASE mydb;
-CREATE USER 'user'@'%' IDENTIFIED BY 'user';
-GRANT ALL PRIVILEGES on mydb.* TO 'user'@'%';
-FLUSH PRIVILEGES;
-
-USE mydb;
-
 CREATE TABLE helloWorld (
     id INT PRIMARY KEY AUTO_INCREMENT,
     mensaje VARCHAR(100)
