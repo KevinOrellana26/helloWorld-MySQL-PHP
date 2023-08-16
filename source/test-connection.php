4dabec1b7905bedd3fef59e053ddf078d2b66191
KevinOrellana26 <kevinorellana202@gmail.com>
2023-08-16 13:39:28 +0200

    update

--- source/test-connection.php
+++ source/test-connection.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>test-connection</title>
+    <title>Test-Connection</title>
 </head>
 <body>
     <?php
-
+    echo "<h1>Test-connection to MySQL</h1>";
     $host  = "mysql";
     $port = "3306";
     $user = "user";
@@ -20,16 +20,9 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    echo "Connected successfully to MySQL";
+    echo "<h3>Connected successfully to MySQL</h3>";
 
     $conn->close();
-
-    // $mysqli = new mysqli("mysql", "user", "password", "mydb");
-    // if ($mysqli->connect_error) {
-    //     echo "Error al realizar la conexión";
-    // }
-    // echo "connected successfully";
-    
     ?>
 </body>
 </html>
